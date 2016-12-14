18f4bb6ef61a42f377d5acdc445ec7c65b8399fc
Shota Morimoto <shouta.morimoto@gmail.com>
2016-12-14 08:03:47 +0000

    Added create/delete network wrapper

---
+++ lib/aviator/openstack/network/requests/v2/public/create_esi_network.rb
@@ -0,0 +1,31 @@
+module Aviator
+  define_request :create_esi_network,
+    :inherit => [:openstack, :common, :'v2', :public, :base] do
+    meta :service, :network
+
+    param :name,            required: false
+    param :description,     required: false
+    param :plane,           required: false
+    param :tenant_id,       required: false
+    param :admin_state_up,  required: false
+
+    def headers
+      super
+    end
+
+    def body
+      p = { network: {} }
+      optional_params.each do |key|
+        p[:network][key] = params[key] if params[key]
+      end
+    end
+
+    def http_method
+      :post
+    end
+
+    def url
+      "#{base_url}/networks"
+    end
+  end
+end
